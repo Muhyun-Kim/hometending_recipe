980f2f904d783a10b7cf55422018d736d4584d54
Muhyun-Kim <muhyun.kim.dev@gmail.com>
2023-07-11 19:02:48 +0900

    add floating button

--- lib/views/components/home_page/home_page_body.dart
+++ lib/views/components/home_page/home_page_body.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
-import '../../recipe_list_page.dart';
+import '../add_recipe_page/show_add_recipe_modal.dart';
+import '../home_recipe_list_page/home_recipe_list_page.dart';
 
 class HomePageBody extends StatefulWidget {
   const HomePageBody({super.key});
@@ -13,22 +14,11 @@ class _HomePageBodyState extends State<HomePageBody> {
   int _currentIndex = 0;
 
   List<Widget> pages = [
-    const RecipeListPage(),
-    const SizedBox(
-      height: double.infinity,
-      width: double.infinity,
-      child: Center(child: Text('Second Page')),
-    ),
+    const HomeRecipeListPage(),
     const SizedBox(
       height: double.infinity,
       width: double.infinity,
       child: Center(child: Text('Third Page')),
-    ),
-    Container(
-      height: double.infinity,
-      width: double.infinity,
-      color: Colors.orange[100],
-      child: const Center(child: Text('Forth Page')),
     ),
   ];
 
@@ -38,9 +28,11 @@ class _HomePageBodyState extends State<HomePageBody> {
       body: SafeArea(child: pages[_currentIndex]),
       bottomNavigationBar: NavigationBar(
         selectedIndex: _currentIndex,
-        onDestinationSelected: (index) => setState(() {
-          _currentIndex = index;
-        }),
+        onDestinationSelected: (index) {
+          setState(() {
+            _currentIndex = index;
+          });
+        },
         destinations: const [
           NavigationDestination(
             selectedIcon: Icon(Icons.home),
@@ -48,16 +40,18 @@ class _HomePageBodyState extends State<HomePageBody> {
             label: 'ホーム',
           ),
           NavigationDestination(
-            selectedIcon: Icon(Icons.post_add),
-            icon: Icon(Icons.post_add),
-            label: '投稿',
-          ),
-          NavigationDestination(
             selectedIcon: Icon(Icons.person),
             icon: Icon(Icons.person),
             label: 'プロファイル',
           ),
         ],
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          showAddRecipeModal(context);
+        },
+        child: const Icon(Icons.add),
       ),
     );
   }

56680e260aa2c90554d6f5124c78ea8fa61760a0
Muhyun-Kim <muhyun.kim.dev@gmail.com>
2023-08-05 21:24:58 +0900

    add my profile

--- lib/views/components/my_profile_page/my_profile_page.dart
+++ lib/views/components/my_profile_page/my_profile_page.dart
@@ -2,10 +2,6 @@ import 'dart:io';
 
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
-import 'package:image_network/image_network.dart';
-
-import '../../../domain/provider/auth_provider.dart';
-import '../../../domain/provider/user_info_provider.dart';
 
 class MyProfilePage extends ConsumerStatefulWidget {
   const MyProfilePage({super.key});
@@ -17,11 +13,6 @@ class MyProfilePage extends ConsumerStatefulWidget {
 class _MyProfilePageState extends ConsumerState<MyProfilePage> {
   @override
   Widget build(BuildContext context) {
-    final currentUser = ref.watch(currentUserProvider)!;
-    final currentUserInfo = ref.watch(userInfoStateProvider)!;
-    String defaultImageUrl =
-        "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png";
-
     File? image;
     void _pickNewProfileImage() {
       print("pick new profile image");
@@ -35,13 +26,6 @@ class _MyProfilePageState extends ConsumerState<MyProfilePage> {
             children: [
               ClipRRect(
                 borderRadius: BorderRadius.circular(60),
-                child: ImageNetwork(
-                  image: currentUserInfo.profileImage.isEmpty
-                      ? defaultImageUrl
-                      : currentUserInfo.profileImage,
-                  height: 120,
-                  width: 120,
-                ),
               ),
               Positioned(
                 bottom: 0,
